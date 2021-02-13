f3e29093a3b0063de6cabf983f36f128aad3ec97
KaqueLimaNovaes <kaiquenovaes.lima@gmail.com>
2021-02-13 18:53:54 -0300

    neon menu

--- src/components/layout/menu/Menu.js
+++ src/components/layout/menu/Menu.js
@@ -1,22 +1,35 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { useHistory } from 'react-router-dom';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 export const Menu = () => {
 
-    const Title = styled.h1`
-        font-size: 20px;
-        font-weight: bold;
-        color: #1d6bff;
+  const fadeIn = keyframes`
+      from {
+        text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #0ae0fc, 0 0 40px #0ae0fc, 0 0 50px #0ae0fc, 0 0 60px #0ae0fc, 0 0 70px #0ae0fc, 0 0 80px #0ae0fc;
+      }
+      to {
+        text-shadow: 0 0 20px #fff, 0 0 30px #0247c9, 0 0 40px #0247c9, 0 0 50px #0247c9, 0 0 60px #0247c9, 0 0 70px #0247c9, 0 0 80px #0247c9;
+      }
+    `;  
+  
+  const Title = styled.h1`
+      font-size: 20px;
+      font-weight: bold;
+      color: #1d6bff;
     `;
 
     const ItensMenu = styled(Nav.Link)`
-        font-size: 20px;
-        color: #1d6bff !important;
+      font-size: 20px;
+      color: #1d6bff !important;
+      &:hover{
+        animation: 2s ${fadeIn} ease-out;
+        color: #0ae0fc !important;
+      }
     `;
 
     const Toggle = styled(Navbar.Toggle)`
-        background-color: #1d6bff !important;
+      background-color: #1d6bff !important;
     `;
 
     const history = useHistory();
