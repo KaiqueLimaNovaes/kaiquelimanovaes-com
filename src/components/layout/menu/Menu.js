881068154f7ca7a489dc4072297208c02db74a99
KaiqueLimaNovaes <kaiquenovaes.lima@gmail.com>
2021-03-02 11:13:43 -0300

    Att hover itens menu

--- src/components/layout/menu/Menu.js
+++ src/components/layout/menu/Menu.js
@@ -4,20 +4,12 @@ import styled, { keyframes } from 'styled-components';
 
 export const Menu = () => {
 
-  const fadeIn = keyframes`
-    from {
-      text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #0ae0fc, 0 0 40px #0ae0fc, 0 0 50px #0ae0fc, 0 0 60px #0ae0fc, 0 0 70px #0ae0fc, 0 0 80px #0ae0fc;
-    }
-    to {
-      text-shadow: 0 0 20px #fff, 0 0 30px #0247c9, 0 0 40px #0247c9, 0 0 50px #0247c9, 0 0 60px #0247c9, 0 0 70px #0247c9, 0 0 80px #0247c9;
-    }
-  `; 
-
   const BarraMenu = styled(Navbar)`
     background-color: #151516 !important;
   `;
   
   const Title = styled.h1`
+      margin: 7px;
       font-size: 25px;
       font-weight: bold;
       color: #A1A1A6;
@@ -30,11 +22,13 @@ export const Menu = () => {
     `;
 
     const ItensMenu = styled(Nav.Link)`
-      font-size: 20px;
+      font-size: 23px;
+      font-weight: bold;
+      padding: 0px;
       color: #A1A1A6 !important;
       &:hover{
-        animation: 2s ${fadeIn} ease-out;
-        color: #0ae0fc !important;
+        text-shadow: -1px -1px 0 rgba(255,255,255,.5), 1px 1px 0 rgba(0,0,0,.25), 3px 3px 3px rgba(0,0,0,.25), 10px 10px 20px rgba(0,0,0,.5), 20px 40px 40px rgba(0,0,0,.5);
+        color: #641E43 !important;
       }
     `;
 
