0fffdeef3543368c5ebaa02768d46ae77ebdde18
KaiqueLimaNovaes <kaiquenovaes.lima@gmail.com>
2021-03-02 10:40:22 -0300

    Cards mobile

---
+++ src/components/layout/card/CardM.js
@@ -0,0 +1,130 @@
+import { Row, Col } from "react-bootstrap";
+import styled, { keyframes } from 'styled-components';
+
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+const animate = keyframes`
+    0%{
+        background-position: 0 0;
+    }
+    50%{
+        background-position: 300% 0;
+    }
+    100%{
+        background-position: 0 0;
+    }
+`; 
+
+const CardC = styled.div`
+    &{  
+        position: relative;
+        margin-top: 40px;
+        margin-right: auto;
+        margin-left: auto;
+        width: 90%;
+        height: 400px;
+        border-radius: 20px;
+        background: linear-gradient(135deg, #651E43, #091A33);
+        cursor: pointer;
+    }
+    &:after,
+    &:before {
+        content: "";
+        position: absolute;
+        top: -2px;
+        left: -2px;
+        background: linear-gradient(45deg, #fb0094, #0000ff, #00ff00, #ffff00, #ff0000, #fb0094, #0000ff, #00ff00, #ffff00, #ff0000);
+        background-size: 400%;
+        width: calc(100% + 4px);
+        height: calc(100% + 4px);
+        border-radius: 20px;
+        z-index: -1;
+        animation: ${animate} 50s linear infinite;
+    }
+    &:after {
+        filter: blur(15px);
+    }
+    @media (max-width: 800px) {
+        height: 800px;
+    }
+`;
+
+const ImgExM = styled.img`
+    width: 214px;
+    height: 380px;
+    @media (max-width: 800px) {
+        max-width: 300px;
+    }
+`;
+
+const CrsM = styled(Carousel)`
+    width: 214px;
+    height: 380px;
+    margin-right: auto;
+    margin-left: auto;
+    @media (max-width: 1200px) {
+        max-width: 300px;
+    }
+`;
+
+const Linha = styled(Row)`
+    height: 100%;
+    margin-top: auto;
+    margin-bottom: auto;
+`;
+
+const Coluna = styled(Col)`
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    padding: 0;
+`;
+
+const TituloCard = styled.h3`
+    margin-right: 40px;
+    margin-left: 40px;
+    font-size: 28px;
+    color: #A1A1A6;
+`;
+
+const TextoCard = styled.h3`
+    margin-right: 40px;
+    margin-left: 40px;
+    font-size: 24px;
+    color: #A1A1A6;
+`;
+
+const BotaoCard = styled.button`
+    width: 300px;
+    height: 50px;
+    border-radius: 25px;
+    background-color: white;
+    font-family:SF Pro Display Semibold;
+    font-size: 20px;
+`;
+
+const CardM = (props) => {
+    return(
+      <CardC onClick={() => window.open(props.end)}>
+          <Linha>
+              <Coluna xl={5}>
+                  <CrsM showArrows={false} infiniteLoop={true} autoPlay={true} showThumbs={false} showStatus={false} showIndicators={false}>
+                    <ImgExM src={props.img1} alt="" />
+                    <ImgExM src={props.img2} alt="" />
+                    <ImgExM src={props.img3} alt="" />
+                  </CrsM>
+              </Coluna>
+              <Coluna xl={7}>
+                <TituloCard>{props.titulo}</TituloCard>
+                <TextoCard>{props.texto}</TextoCard>
+                <BotaoCard onClick={() => window.open(props.aplicacao)}>Visualizar</BotaoCard>
+              </Coluna>
+          </Linha>
+      </CardC>
+    );
+}
+
+export default CardM;
--- src/pages/mobile/Mobile.js
+++ src/pages/mobile/Mobile.js
@@ -1,6 +1,13 @@
 import styled from 'styled-components';
 
-import Card from '../../components/layout/card/Card';
+import CardM from '../../components/layout/card/CardM';
+
+import AsseguraApp1 from '../../assets/images/mobile/AsseguraApp1.jpeg';
+import AsseguraApp2 from '../../assets/images/mobile/AsseguraApp2.jpeg';
+import AsseguraApp3 from '../../assets/images/mobile/AsseguraApp3.jpeg';
+import FullstackApp1 from '../../assets/images/mobile/FullstackApp1.jpeg';
+import FullstackApp2 from '../../assets/images/mobile/FullstackApp2.jpeg';
+import FullstackApp3 from '../../assets/images/mobile/FullstackApp3.jpeg';
 
 const Section1 = styled.div`
   justify-content: center;
@@ -17,9 +24,16 @@ const Mobile = () => {
     return(
       <Section1>
         <Title>Mobile</Title>
-        <Card />
-        <Card />
-        <Card />
+
+        <CardM texto={'Um projeto criado em React native com banco de dados sql tudo conectado por um back-end em Nodejs, realizado em parceiria com mais 5 Devs.'}
+        img1={AsseguraApp1} img2={AsseguraApp2} img3={AsseguraApp3} 
+        titulo={'App Assegura LGBTQIA+'} end={'https://github.com/squad4spm/Assegura-Lgbtqia-App.git'}
+        aplicacao={'https://github.com/squad4spm/Assegura-Lgbtqia-App.git'} />
+        
+        <CardM texto={'Um projeto criado em Reactjs com ssr em Nextjs, com estilização através de styled components e responsividade com react-bootstrap.'}
+        img1={FullstackApp1} img2={FullstackApp2} img3={FullstackApp3}
+        titulo={'App Fullstack games'} end={'https://github.com/KaiqueLimaNovaes/fullstack-games-app.git'}
+        aplicacao={'https://github.com/KaiqueLimaNovaes/fullstack-games-app.git'} />
       </Section1>
     );
 }
